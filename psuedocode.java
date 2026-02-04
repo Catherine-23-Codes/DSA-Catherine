cabfb871aac2ff1c0d16cd801b8cd14736711476
CATHERINE INFANTA  A <infantacatherine398@gmail.com>
2026-02-04 21:37:16 +0530

    notes

---
+++ psuedocode.java
@@ -0,0 +1,13 @@
+
+/*
+public class psuedocode {
+    public static String recursionMethod(String[]parameters){
+        if(exit from condition){
+            return some_value;
+        }else{
+           System.out.println(recursionMethod(parameters));
+        }
+    }
+}
+
+*/
